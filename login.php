2b837b0686c865eb2d6ebb63276b37ff26a60ba4
lx5971367 <lx5971367@126.com>
2016-09-07 00:13:15 +0800

    取消登录界面的表格边框

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
 	</head>
 	<h1>管理员登录系统</h1>
 	<form action="loginProcess.php" method="post">
-		<table border="1">
+		<table>
 			<tr><td>用户ID</td><td><input type="text" name="id"></td></tr>
 			<tr><td>密&nbsp码</td><td><input type="password" name="password"></td></tr>
 			<tr><td><input type="submit" value="提交"></td><td><input type="reset" value="重新输入"></td></tr>
