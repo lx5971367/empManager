4f0ae86575816ee6e743bc8a36595b6e5b3d287e
lx5971367 <lx5971367@126.com>
2016-09-07 00:10:16 +0800

    写好登录界面，正在写loginProcess接收数据

--- login.php
+++ login.php
@@ -2,4 +2,12 @@
 	<head>
 		<meta http-equiv="content-type" content="text/html;charset=utf-8"/>
 	</head>
+	<h1>管理员登录系统</h1>
+	<form action="loginProcess.php" method="post">
+		<table border="1">
+			<tr><td>用户ID</td><td><input type="text" name="id"></td></tr>
+			<tr><td>密&nbsp码</td><td><input type="password" name="password"></td></tr>
+			<tr><td><input type="submit" value="提交"></td><td><input type="reset" value="重新输入"></td></tr>
+		</table>
+	</form>
 </html>
---
+++ loginProcess.php
@@ -0,0 +1,5 @@
+<?php
+	//接收用户数据
+	$id=$_POST['id'];
+	$password=$_POST['password'];
+?>
