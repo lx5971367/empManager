401c0dcba50108d587d871a2ea3d08ffad64e3d3
lx5971367 <lx5971367@126.com>
2016-09-07 11:39:14 +0800

    初步简单验证，尚未连接数据库

--- loginProcess.php
+++ loginProcess.php
@@ -2,4 +2,14 @@
 	//接收用户数据
 	$id=$_POST['id'];
 	$password=$_POST['password'];
+	
+	//简单验证，不到数据库
+	if($id==1 && $password=="admin")
+	{
+	    header("Location:empManager.php");
+	}
+	else 
+	{
+	    header("Location:login.php");
+	}
 ?>
