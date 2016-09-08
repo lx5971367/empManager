2fd0ffa98dea8206bafe8732058af7a1da53b496
lx5971367 <lx5971367@126.com>
2016-09-08 16:43:12 +0800

    改用数据库的方式验证用户名密码

--- empManager.php
+++ empManager.php
@@ -1,3 +1,14 @@
+<html>
+	<head>
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	</head>
+	<h1>主界面</h1>
+		<a href="#">管理用户</a></br>
+		<a href="#">添加用户</a></br>
+		<a href="#">删除用户</a></br>
+		<a href="#">退出系统</a></br>
+</html>
+
 <?php
 	echo "登录成功";
 	echo "</br><a href='login.php'>返回登录页面</a>"
--- loginProcess.php
+++ loginProcess.php
@@ -4,7 +4,7 @@
 	$password=$_POST['password'];
 	
 	//简单验证，不到数据库
-	if($id==1 && $password=="admin")
+	/*if($id==1 && $password=="admin")
 	{
 	    header("Location:empManager.php");
 	    //如果要跳转了，则最好带上exit();
@@ -13,5 +13,24 @@
 	else 
 	{
 	    header("Location:login.php?error=1");//携带一个信息回去
+	}*/
+	
+	//用数据库的方法进行验证
+	$mysqli=new MySQLi("localhost","root","lx128SIMON","study");
+	if($mysqli->connect_error)
+	{
+	    die("连接失败".$mysqli->connect_error);   
 	}
+	$mysqli->query("set names utf8");
+	$res=$mysqli->query("select password from admin where id = $id");
+	if($row=$res->fetch_assoc())
+	{
+	    if($row['password'] == $password)
+	    {
+	        header("Location:empManager.php");
+	        exit();
+	    }
+	}
+	header("Location:login.php?error=1");
+	exit();
 ?>
